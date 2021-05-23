4e8101896eb4308f16fbe1c042857bb4be591de9
EDusik <eduardodusik@gmail.com>
2021-05-23 15:35:47 -0300

    02-03 - template and data

--- 02 - vue-basics/app.js
+++ 02 - vue-basics/app.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
   // data, functions, templates
   // template: '<h2>I am the template</h2>'
+  data() {
+    return {
+      title: 'The final empire',
+      author: 'Brandon Sanderson',
+      age: 45
+    }
+  }
 })
 
 app.mount('#app')

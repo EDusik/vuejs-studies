c224131efc15bb6ca86b3792377c7e719b2114cc
EDusik <eduardodusik@gmail.com>
2021-05-23 18:30:40 -0300

    02-07 - 'v-for'

--- 02 - vue-basics/app.js
+++ 02 - vue-basics/app.js
@@ -8,7 +8,12 @@ const app = Vue.createApp({
       author: 'Brandon Sanderson',
       age: 45,
       x: 0,
-      y: 0
+      y: 0,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss'},
+        { title: 'the way of kings', author: 'brandon sanderson'},
+        { title: 'the final empire', author: 'brandon sanderson'}
+      ]
     }
   },
   methods: {

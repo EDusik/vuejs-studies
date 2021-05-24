ef5c4b9f6f7b725ab84347df89078cbb76d5b38e
EDusik <eduardodusik@gmail.com>
2021-05-24 09:56:08 -0300

    02-08 - attribute binding

--- 02 - vue-basics/app.js
+++ 02 - vue-basics/app.js
@@ -9,10 +9,11 @@ const app = Vue.createApp({
       age: 45,
       x: 0,
       y: 0,
+      url: "https://eduardodusik.dev/",
       books: [
-        { title: 'name of the wind', author: 'patrick rothfuss'},
-        { title: 'the way of kings', author: 'brandon sanderson'},
-        { title: 'the final empire', author: 'brandon sanderson'}
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg' },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg' },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg' }
       ]
     }
   },

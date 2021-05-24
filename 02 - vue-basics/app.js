890257423ed9607dbed5a80d711c34ea084c95c8
EDusik <eduardodusik@gmail.com>
2021-05-24 10:05:40 -0300

    02-09 - dynamic classes

--- 02 - vue-basics/app.js
+++ 02 - vue-basics/app.js
@@ -11,9 +11,9 @@ const app = Vue.createApp({
       y: 0,
       url: "https://eduardodusik.dev/",
       books: [
-        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg' },
-        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg' },
-        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg' }
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: false }
       ]
     }
   },

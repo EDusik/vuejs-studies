bc141b510100f43ada0b7169dddde337c2746b33
EDusik <eduardodusik@gmail.com>
2021-05-26 19:31:14 -0300

    32 - starting a new game

--- 04-project-build/reaction-timer/src/App.vue
+++ 04-project-build/reaction-timer/src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <h1>Ninja reaction timer</h1>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
+import Block from './components/Block.vue';
 
 export default {
   name: 'App',
-  components: { },
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+  },
 };
 </script>
 

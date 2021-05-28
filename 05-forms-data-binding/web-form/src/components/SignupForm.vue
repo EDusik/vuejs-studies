719e79efbe7df5b9da49434ed3cdb3f13a6508c9
EDusik <eduardodusik@gmail.com>
2021-05-27 21:10:05 -0300

    40 - select fields

--- 05-forms-data-binding/web-form/src/components/SignupForm.vue
+++ 05-forms-data-binding/web-form/src/components/SignupForm.vue
@@ -5,6 +5,12 @@
 
     <label>Password:</label>
     <input type="password" required v-model="password" />
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
   </form>
 </template>
 
@@ -13,7 +19,8 @@ export default {
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      role: "developer"
     }
   }
 }
@@ -39,7 +46,7 @@ export default {
     font-weight: bold;
   }
 
-  input {
+  input, select {
     display: block;
     padding: 10px 6px;
     width: 100%;

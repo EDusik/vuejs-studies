fd9bf34c10338bb1c547cb24e18e0abe5553a3a8
EDusik <eduardodusik@gmail.com>
2021-06-03 16:52:45 -0300

    module 07

---
+++ 07-fetching-data/ninja-jobs/src/router/index.js
@@ -0,0 +1,50 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import NotFound from '../views/NotFound.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/jobs',
+    name: 'Jobs',
+    component: Jobs
+  }
+  ,
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // catchAll 404 
+  { 
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ 07-fetching-data/ninja-jobs/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni est in, accusantium enim, quibusdam totam fugiat deleniti aperiam inventore, a ea commodi pariatur suscipit incidunt doloribus minus explicabo modi facere nulla velit ipsam quae amet eaque. Eveniet minima quos voluptatum blanditiis officia, quam ipsa atque adipisci odit quidem, facere minus! Labore velit eligendi ad eum odio modi vero natus dicta consequuntur necessitatibus quod fugiat tempora minus doloribus aliquam exercitationem repellendus, voluptate in adipisci omnis odit, vel impedit corporis? Voluptate eligendi non culpa optio? Sed, ab nam tenetur voluptas deleniti atque esse doloremque reprehenderit accusamus, ipsum, quod dolorum necessitatibus aut a!</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni est in, accusantium enim, quibusdam totam fugiat deleniti aperiam inventore, a ea commodi pariatur suscipit incidunt doloribus minus explicabo modi facere nulla velit ipsam quae amet eaque. Eveniet minima quos voluptatum blanditiis officia, quam ipsa atque adipisci odit quidem, facere minus! Labore velit eligendi ad eum odio modi vero natus dicta consequuntur necessitatibus quod fugiat tempora minus doloribus aliquam exercitationem repellendus, voluptate in adipisci omnis odit, vel impedit corporis? Voluptate eligendi non culpa optio? Sed, ab nam tenetur voluptas deleniti atque esse doloremque reprehenderit accusamus, ipsum, quod dolorum necessitatibus aut a!</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: { }
+}
+</script>

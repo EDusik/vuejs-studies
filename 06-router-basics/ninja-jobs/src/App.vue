751de6d048ec9a4ffb3a2feb3b6548190bdd2f8e
EDusik <eduardodusik@gmail.com>
2021-06-03 14:53:43 -0300

    52 - programmatic navigation

--- 06-router-basics/ninja-jobs/src/App.vue
+++ 06-router-basics/ninja-jobs/src/App.vue
@@ -4,9 +4,32 @@
     <router-link :to="{ name: 'About' }">About</router-link>
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </div>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+
   <router-view/>
 </template>
 
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({
+        name: 'Home'
+      })
+    },
+    back() {
+      // this.$router.back()
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go(1)
+    }
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -33,5 +56,13 @@
 #nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+
+button {
+  cursor: pointer;
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>

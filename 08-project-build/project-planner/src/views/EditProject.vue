f201731ab63478474fe4b36116c8f15d9d30b885
EDusik <eduardodusik@gmail.com>
2021-06-04 15:52:53 -0300

    67 - updating projects

--- 08-project-build/project-planner/src/views/EditProject.vue
+++ 08-project-build/project-planner/src/views/EditProject.vue
@@ -15,16 +15,32 @@ export default {
     return {
       title: '',
       details: '',
-      uri: 'http://localhost:3000/projects/'
+      uri: 'http://localhost:3000/projects'
     }
   },
   mounted() {
-    fetch(`${this.uri}${this.id}`)
+    fetch(`${this.uri}/${this.id}`)
       .then(res => res.json())
       .then(data => {
         this.title = data.title,
         this.details = data.details
-      })
+    }).catch(err => console.log(err));
+  },
+  methods: {
+    handleSubmit() {
+      const editedProject = {
+        title: this.title,
+        details: this.details
+      }
+
+      fetch(`${this.uri}/${this.id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(editedProject)
+      }).then(() => {
+        this.$router.push('/');
+      }).catch(err => console.log(err));
+    }
   }
 }
 </script>

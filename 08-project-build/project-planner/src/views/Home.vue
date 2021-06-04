8ef2213845b77a088fab338c287061cab9efb63d
EDusik <eduardodusik@gmail.com>
2021-06-04 17:29:49 -0300

    69 - showing filtered projects

--- 08-project-build/project-planner/src/views/Home.vue
+++ 08-project-build/project-planner/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <FilterNav @filterChange="current = $event" :current="current" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
       </div>
     </div>
@@ -41,6 +41,18 @@ export default {
 
       p.complete = !p.complete;
     },
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === 'completed') {
+        return this.projects.filter(project => project.complete)
+      }
+
+      if (this.current === 'ongoing') {
+        return this.projects.filter(project => !project.complete)
+      }
+      return this.projects;
+    }
   }
 }
 </script>

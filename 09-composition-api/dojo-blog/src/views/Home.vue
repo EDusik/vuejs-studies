4627b0abf7f37d7b1f72d0ae0d4aca71e80423d6
EDusik <eduardodusik@gmail.com>
2021-06-07 18:58:33 -0300

    71 - composition API basics

---
+++ 09-composition-api/dojo-blog/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Home</h1>
+  <p>My name is {{ name }} and my age is {{ age }} </p>
+  <button @click="handleClick">click me</button>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  setup() {
+    const name = 'Eduardo'
+    const age = 31
+
+    const handleClick = () => {
+      console.log('you clicked me')
+    }
+
+    return {
+      name,
+      age, 
+      handleClick
+    }
+  }
+}
+</script>

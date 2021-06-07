f7e6b557db2961de7e47407606b86ab465d50c03
EDusik <eduardodusik@gmail.com>
2021-06-07 19:46:13 -0300

    73 - using refs for reactivity

--- 09-composition-api/dojo-blog/src/views/Home.vue
+++ 09-composition-api/dojo-blog/src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <h1>Home</h1>
-  <p ref="p">My name is {{ name }} and my age is {{ age }} </p>
+  <p>My name is {{ name }} and my age is {{ age }} </p>
   <button @click="handleClick">click me</button>
+  <button @click="age++">add one to age</button>
+  <input type="text" v-model="name" />
 </template>
 
 <script>
@@ -11,22 +13,23 @@ export default {
   name: 'Home',
   setup() {
 
-    const p = ref(null);
+    // const p = ref(null);
 
-    const name = 'Eduardo'
-    const age = 31
+    const name = ref('Eduardo')
+    const age = ref(31)
 
     const handleClick = () => {
-      console.log(p, p.value)
-      p.value.classList.add('test')
-      p.value.textContent = 'Hello, ninjas!'
+      // console.log(p, p.value)
+      // p.value.classList.add('test')
+      // p.value.textContent = 'Hello, ninjas!'
+      name.value = 'Eduardo Dusik'
+      age.value = 32
     }
 
     return {
       name,
       age, 
-      handleClick, 
-      p
+      handleClick,
     }
   }
 }

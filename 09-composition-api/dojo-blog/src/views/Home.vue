0a117ea395b96b4f5125983ec04a93cd4ca59860
EDusik <eduardodusik@gmail.com>
2021-06-08 19:21:46 -0300

    76 - using watch and watchEffect

--- 09-composition-api/dojo-blog/src/views/Home.vue
+++ 09-composition-api/dojo-blog/src/views/Home.vue
@@ -17,47 +17,69 @@
 </template>
 
 <script>
-import { ref, reactive, computed } from 'vue'
+import { ref, reactive, computed, watch, watchEffect } from 'vue'
 
 export default {
   name: 'Home',
   setup() {
 
-    // const p = ref(null);
+  // const p = ref(null);
 
-    // const name = ref('Eduardo')
-    // const age = ref(31)
+  // const name = ref('Eduardo')
+  // const age = ref(31)
 
-    // const ninjaOne = ref({ name: 'edu', age: 30 })
-    // const ninjaTwo = reactive({ name: 'dudu', age: 31})
+  // const ninjaOne = ref({ name: 'edu', age: 30 })
+  // const ninjaTwo = reactive({ name: 'dudu', age: 31})
 
-    // const updateNinjaOne = () => {
-    //   ninjaOne.value.age = 40;
-    // }
+  // const updateNinjaOne = () => {
+  //   ninjaOne.value.age = 40;
+  // }
 
-    // const updateNinjaTwo = () => {
-    //   ninjaTwo.age = 32;
-    // }
+  // const updateNinjaTwo = () => {
+  //   ninjaTwo.age = 32;
+  // }
 
-    // const name = computed(() => {
-    //   return 'Eduardo Dusik'
-    // })
+  // const name = computed(() => {
+  //   return 'Eduardo Dusik'
+  // })
 
-    const search = ref('')
-    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
-    // const handleClick = () => {
-    //   // console.log(p, p.value)
-    //   // p.value.classList.add('test')
-    //   // p.value.textContent = 'Hello, ninjas!'
-    //   name.value = 'Eduardo Dusik'
-    //   age.value = 32
-    // }
+  const search = ref('')
+  const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
+  // const handleClick = () => {
+  //   // console.log(p, p.value)
+  //   // p.value.classList.add('test')
+  //   // p.value.textContent = 'Hello, ninjas!'
+  //   name.value = 'Eduardo Dusik'
+  //   age.value = 32
+  // }
+
+  watch(search, () => {
+    console.log(search)
+  });
+
+  const stopWatch = watch(() => {
+    console.log('watch function ran', search.value)
+  })
+
+  watchEffect(() => {
+    console.log('watch function ran', search.value)
+  })
+
+  const stopEffect = watchEffect(() => {
+    console.log('watch function ran', search.value)
+  })
 
   const matchingNames = computed(() => {
-      // return ['a', 'b', 'c']
-      return names.value.filter(name => name.includes(search.value))
-    })
-    return { names, search, matchingNames }
+    // return ['a', 'b', 'c']
+    return names.value.filter(name => name.includes(search.value))
+  })
+
+  const handleClick = () => {
+    stopWatch()
+    stopEffect()
+  }
+
+  return { names, search, matchingNames }
 
     // return {
     //   name,

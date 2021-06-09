95711da760d85e5cd6fc277d6b57d536c5fb537e
EDusik <eduardodusik@gmail.com>
2021-06-09 19:34:24 -0300

    79 - fetching data in the setup function

--- 10-async-code/dojo-blog/src/views/Home.vue
+++ 10-async-code/dojo-blog/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <h1>Home</h1>
-  <PostList :posts="posts" />
+  <div v-if="error">{{ error }}</div>
+  <div v-if="posts.length">
+    <PostList :posts="posts" />
+  </div>
+  <div v-else>Loading...</div>
 </template>
 
 <script>
@@ -11,12 +15,26 @@ export default {
   name: 'Home',
   components: { PostList },
   setup() {
-    const posts = ref([
-      { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1 },
-      { title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2 }
-    ])
+    const posts = ref([])
+    const error = ref(null)
 
-    return { posts }
+    const load = async () => {
+      try {
+        const data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw new Error('no data available')
+        }
+
+        posts.value = await data.json();
+      }
+      catch (err) {
+        error.value = err.message
+      }
+    }
+
+    load()
+
+    return { posts, error }
   }
 }
 </script>

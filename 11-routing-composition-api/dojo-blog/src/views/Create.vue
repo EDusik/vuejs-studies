2f93098ba88460427e4606056e71def64cc2fd2f
EDusik <eduardodusik@gmail.com>
2021-06-13 18:42:43 -0300

    87 - adding useRouter()

--- 11-routing-composition-api/dojo-blog/src/views/Create.vue
+++ 11-routing-composition-api/dojo-blog/src/views/Create.vue
@@ -21,6 +21,7 @@
 
 <script>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default {
   setup() {
@@ -28,7 +29,9 @@ export default {
     const body = ref('');
     const tag = ref('');
 
-    const tags = ref([])
+    const tags = ref([]);
+
+    const router = useRouter();
 
     const handleKeydown = () => {
       if (!tags.value.includes(tag.value)) {
@@ -50,6 +53,8 @@ export default {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(post) 
       })
+
+      router.push({ name: 'Home' });
     }
 
     return { title, body, tag, handleKeydown, tags, handleSubmit }

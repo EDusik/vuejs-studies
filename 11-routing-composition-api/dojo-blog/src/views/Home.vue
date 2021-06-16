cae8cc5fd4754decfae450f9a1e746977b4132c5
EDusik <eduardodusik@gmail.com>
2021-06-16 20:00:38 -0300

    90 - creating a tag cloud

--- 11-routing-composition-api/dojo-blog/src/views/Home.vue
+++ 11-routing-composition-api/dojo-blog/src/views/Home.vue
@@ -2,8 +2,9 @@
   <div class="home">
     <h1>Home</h1>
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="posts" />
+      <TagCloud :posts="posts" />
     </div>
     <div v-else><Spinner /></div>
   </div>
@@ -13,10 +14,11 @@
 import PostList from '../components/PostList.vue'
 import getPosts from '../composables/getPosts'
 import Spinner from '../components/Spinner.vue'
+import TagCloud from '../components/TagCloud.vue'
 
 export default {
   name: 'Home',
-  components: { PostList, Spinner },
+  components: { PostList, Spinner, TagCloud },
   setup() {
     const { posts, error, load } = getPosts();
 
@@ -31,5 +33,11 @@ export default {
     max-width: 1200px;
     margin: 0 auto;
     padding: 10px;
+  }
+
+  .layout {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    gap: 20px;
   }
 </style>
--- 11-routing-composition-api/dojo-blog/src/views/Tag.vue
+++ 11-routing-composition-api/dojo-blog/src/views/Tag.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="tag">
-    <h1>Tags: {{ tags }}</h1>
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="filteredPosts" />
+      <TagCloud :posts="posts" />
     </div>
     <div v-else>
       <Spinner />
@@ -13,6 +13,7 @@
 
 <script>
 import PostList from '../components/PostList.vue'
+import TagCloud from '../components/TagCloud.vue'
 import Spinner from "../components/Spinner.vue";
 import getPosts from "../composables/getPosts";
 
@@ -22,28 +23,32 @@ import { useRoute } from "vue-router";
 
 export default {
   props: ["tags"],
-  components: { Spinner, PostList },
+  components: { Spinner, PostList, TagCloud },
   setup(props) {
     const route = useRoute();
-    const tags = route.params.tags;
-
     const { posts, error, load } = getPosts();
 
     load();
 
     const filteredPosts  = computed(() => {
-      return posts.value.filter((post) => post.tags.includes(tags))
+      return posts.value.filter((post) => post.tags.includes(route.params.tags))
     })
 
-    return { error, tags, posts, filteredPosts };
+    return { error, posts, filteredPosts };
   },
 };
 </script>
 
 <style>
-.tag {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 10px;
-}
+  .tag {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px;
+  }
+
+  .layout {
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    gap: 20px;
+  }
 </style>

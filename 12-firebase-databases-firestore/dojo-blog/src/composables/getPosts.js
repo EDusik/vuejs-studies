36e92cf66d842e5c48e44fe33659825c9b67fe82
EDusik <eduardodusik@gmail.com>
2021-06-17 21:35:27 -0300

    94 - fetching a collection of documents

--- 12-firebase-databases-firestore/dojo-blog/src/composables/getPosts.js
+++ 12-firebase-databases-firestore/dojo-blog/src/composables/getPosts.js
@@ -1,4 +1,5 @@
 import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const getPosts = () => {
   const posts = ref([])
@@ -6,12 +7,12 @@ const getPosts = () => {
 
   const load = async () => {
     try {
-      const data = await fetch('http://localhost:3000/posts')
-      if (!data.ok) {
-        throw new Error('no data available')
-      }
-
-      posts.value = await data.json();
+      const res = await projectFirestore.collection('posts').get()
+      posts.value = res.docs.map(doc => {
+        return {
+          ...doc.data(), id: doc.id
+        }
+      })
     }
     catch (err) {
       error.value = err.message
---
+++ 12-firebase-databases-firestore/dojo-blog/src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3ftp4tykr0wBn87W6KqqTK8qR4Ezi91o",
+  authDomain: "udemy-vue-firebase-sites-b7175.firebaseapp.com",
+  projectId: "udemy-vue-firebase-sites-b7175",
+  storageBucket: "udemy-vue-firebase-sites-b7175.appspot.com",
+  messagingSenderId: "697043142361",
+  appId: "1:697043142361:web:7b550e9c67702ead1460c8"
+};
+
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init firestore service
+const projectFirestore = firebase.firestore()
+
+// export 
+export { projectFirestore }

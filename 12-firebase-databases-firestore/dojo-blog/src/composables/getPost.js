3d25d794c261e5e8be139ef5268f3898248765e3
EDusik <eduardodusik@gmail.com>
2021-06-18 20:36:06 -0300

    95 - fetching single documents

--- 12-firebase-databases-firestore/dojo-blog/src/composables/getPost.js
+++ 12-firebase-databases-firestore/dojo-blog/src/composables/getPost.js
@@ -1,4 +1,5 @@
 import { ref } from "vue"
+import { projectFirestore } from '../firebase/config'
 
 const getPost = (id) => {
   const post = ref(null)
@@ -6,15 +7,16 @@ const getPost = (id) => {
 
   const load = async () => {
     try {
-      const data = await fetch(`http://localhost:3000/posts/${id}`)
-      if (!data.ok) {
-        throw new Error('that post does not exist')
-      }
+      const res= await projectFirestore.collection('posts').doc(id).get()
 
-      post.value = await data.json();
-    }
-    catch (err) {
+      if (!res.exists) {
+        throw Error('That post does not exist')
+      }
+      
+      post.value = { ...res.data(), id: res.id }
+    } catch (err) {
       error.value = err.message
+      console.log(error.value)
     }
   }
 

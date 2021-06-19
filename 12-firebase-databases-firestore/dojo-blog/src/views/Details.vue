77796955d967a9f7a766388fd862a37526ebb51f
EDusik <eduardodusik@gmail.com>
2021-06-18 21:00:02 -0300

    97. Deleting Docments

--- 12-firebase-databases-firestore/dojo-blog/src/views/Details.vue
+++ 12-firebase-databases-firestore/dojo-blog/src/views/Details.vue
@@ -3,6 +3,7 @@
   <div v-if="post" class="post">
     <h3>{{ post.title }}</h3>
     <p class="pre">{{ post.body }}</p>
+    <button class="delete" @click="handleClick">delete post</button>
   </div>
   <div v-else>
     <Spinner />
@@ -12,18 +13,29 @@
 <script>
 import getPost from '../composables/getPost'
 import Spinner from '../components/Spinner.vue'
-import { useRoute } from 'vue-router'
+
+import { useRoute, useRouter } from 'vue-router'
+import { projectFirestore } from '../firebase/config'
 
 export default {
   props: ['id'],
   components: { Spinner },
   setup(props) {
     const route = useRoute();
+    const router = useRouter();
     const { post, error, load } = getPost(route.params.id)
 
     load();
 
-    return { post, error }
+    const handleClick = async () => {
+      await projectFirestore.collection('posts')
+      .doc(props.id)
+      .delete()
+
+      router.push('/')
+    }
+
+    return { post, error, handleClick }
   }
 }
 </script>
@@ -43,5 +55,9 @@ export default {
   }
   .pre {
     white-space: pre-wrap;
+  }
+
+  button.delete {
+    margin: 10px auto
   }
 </style>

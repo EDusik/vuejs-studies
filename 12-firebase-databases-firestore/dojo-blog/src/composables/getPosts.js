bf83c22d59320aee0f0f21244839bbda0bec4752
EDusik <eduardodusik@gmail.com>
2021-06-18 21:27:09 -0300

    99 - ordering collections

--- 12-firebase-databases-firestore/dojo-blog/src/composables/getPosts.js
+++ 12-firebase-databases-firestore/dojo-blog/src/composables/getPosts.js
@@ -7,7 +7,9 @@ const getPosts = () => {
 
   const load = async () => {
     try {
-      const res = await projectFirestore.collection('posts').get()
+      const res = await projectFirestore.collection('posts')
+      .orderBy('createdAt', 'desc')
+      .get()
       posts.value = res.docs.map(doc => {
         return {
           ...doc.data(), id: doc.id
